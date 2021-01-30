80c0c0cefd94ab38a50b4f4e691bcb2ac068c50a
Adel-Ab <45152570+Adel-Ab@users.noreply.github.com>
2021-01-30 16:08:30 -0500

    MedicalRecord Creation

---
+++ 711/app/src/main/java/com/example/a711/MedicalRecord.java
@@ -0,0 +1,100 @@
+package com.example.a711;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MedicalRecord {
+
+    private Patient patient;
+    // Current Patient Condition
+    private ArrayList<Prescription> prescriptions = new ArrayList();
+    private ArrayList<String> condition = new ArrayList();
+    private ArrayList<String> symptoms = new ArrayList();
+
+    // Patient Notes to be viewed by the Doctor
+    private HashMap<String,String> patientNotes= new HashMap();
+    // Doctor Notes to keep track of patient
+    private HashMap<String,String> doctorNotes= new HashMap();
+
+    // Constructor
+    public MedicalRecord(Patient patient) {
+        this.patient = patient;
+    }
+
+    // Getter Setter
+    public Patient getPatient() {
+        return patient;
+    }
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public ArrayList<Prescription> getPrescriptions(){
+        return prescriptions;
+    }
+    public void setPrescriptions(ArrayList<Prescription> prescriptions){
+        this.prescriptions = prescriptions;
+    }
+    public void addPrescription (Prescription prescription){
+        this.prescriptions.add(prescription);
+    }
+    public void removePrescription (Prescription prescription){
+        this.prescriptions.remove(prescription);
+    }
+
+    public ArrayList<String> getCondition(){
+        return condition;
+    }
+    public void setCondition(ArrayList<String> condition){
+        this.condition = condition;
+    }
+    public void addCondition(String condition) {
+        this.condition.add(condition);
+    }
+    public void removeCondition(String condition){
+        this.condition.remove(condition);
+    }
+
+    public ArrayList<String> getSymptoms(){
+        return symptoms;
+    }
+    public void setSymptoms(ArrayList<String> symptoms){
+        this.symptoms = symptoms;
+    }
+    public void addSymptoms(String symptom) {
+        this.symptoms.add(symptom);
+    }
+    public void removeSymptoms(String symptom){
+        this.symptoms.remove(symptom);
+    }
+
+    public HashMap<String, String> getPatientNotes() {
+        return patientNotes;
+    }
+    public void setPatientNotes(HashMap<String, String> patientNotes) {
+        this.patientNotes = patientNotes;
+    }
+
+    public void addPatientNotes(String date, String note) {
+        this.patientNotes.put(date,note);
+    }
+    public void removePatientNotes(String date){
+        this.patientNotes.remove(date);
+    }
+
+    public HashMap<String, String> getDoctorNotes() {
+        return doctorNotes;
+    }
+    public void setDoctorNotes(HashMap<String, String> doctorNotes) {
+        this.doctorNotes = doctorNotes;
+    }
+
+    public void addDoctorNotes(String date, String note) {
+        this.doctorNotes.put(date,note);
+    }
+    public void removeDoctorNotes(String date){
+        this.doctorNotes.remove(date);
+    }
+
+
+}
--- 711/app/src/main/java/com/example/a711/Patient.java
+++ 711/app/src/main/java/com/example/a711/Patient.java
@@ -1,6 +1,5 @@
 package com.example.a711;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 
 public class Patient {
 
@@ -10,12 +9,6 @@ public class Patient {
     private int age;
     private String email;
     private Doctor doctor;
-    // Current Patient Condition
-    private ArrayList<Prescription> prescriptions = new ArrayList();
-    private ArrayList<String> condition = new ArrayList();
-    private ArrayList<String> symptoms = new ArrayList();
-    // Patient Notes to be viewed by the Doctor
-    private HashMap<String,String> patientNotes= new HashMap();
 
     // Constructor
     public Patient(String id, String name, int age, String email){
@@ -60,45 +53,5 @@ public class Patient {
     public void setDoctor(Doctor doctor){
         this.doctor = doctor;
     }
-
-    public ArrayList<Prescription> getPrescriptions(){
-        return prescriptions;
-    }
-    public void setPrescriptions(ArrayList<Prescription> prescriptions){
-        this.prescriptions = prescriptions;
-    }
-    public void addPrescription (Prescription prescription){
-        this.prescriptions.add(prescription);
-    }
-    public void removePrescription (Prescription prescription){
-        this.prescriptions.remove(prescription);
-    }
-
-    public ArrayList<String> getCondition(){
-        return condition;
-    }
-    public void setCondition(ArrayList<String> condition){
-        this.condition = condition;
-    }
-    public void addCondition(String condition) {
-        this.condition.add(condition);
-    }
-    public void removeCondition(String condition){
-        this.condition.remove(condition);
-    }
-
-    public ArrayList<String> getSymptoms(){
-        return symptoms;
-    }
-    public void setSymptoms(ArrayList<String> symptoms){
-        this.symptoms = symptoms;
-    }
-    public void addSymptoms(String symptom) {
-        this.symptoms.add(symptom);
-    }
-    public void removeSymptoms(String symptom){
-        this.symptoms.remove(symptom);
-    }
-
 
 }
