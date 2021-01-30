1e2509f20a7a7719cd5089d36bc1e53185aa0861
KianSalehi <kian.k.salehi@gmail.com>
2021-01-29 22:58:22 -0500

    PatientSignUp updated

--- 711/app/src/main/java/com/example/a711/PatientSignup.java
+++ 711/app/src/main/java/com/example/a711/PatientSignup.java
@@ -3,6 +3,8 @@ package com.example.a711;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PatientSignup extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class PatientSignup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_signup);
+        Button signUp= (Button) findViewById(R.id.PatientSignupButton);
+        TextView email = (TextView) findViewById(R.id.PatientSignupEmail);
+        TextView password = (TextView) findViewById(R.id.PatientSignupPass);
     }
 }
