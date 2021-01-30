078fe7a34eb096a3fa31688097a8433b133aae6c
Adel-Ab <45152570+Adel-Ab@users.noreply.github.com>
2021-01-30 12:55:19 -0500

    Patient update

--- 711/app/src/main/java/com/example/a711/Patient.java
+++ 711/app/src/main/java/com/example/a711/Patient.java
@@ -1,19 +1,29 @@
 package com.example.a711;
+import java.util.Hashmap;
 
 public class Patient {
 
+    // Patient informtaions
     public String id;
     public String name;
+    public int age;
     public String email;
     public Doctor doctor;
+    // Current Patient Condition
+    public Prescription[] prescriptions = new Prescription[];
+    public String[] condition = new String[];
+    public String[] symptoms = new String[];
+    // Patient Notes to be viewed by the Doctor
+    public Hashmap<String,String> patientNotes= new Hashmap<String, String>;
 
-
+    // Constructor
     public Patient(String id, String name, String email){
         this.setId(id);
         this.setName(name);
         this.setEmail(email);
     };
 
+    // Getter Setter
     public String getId(){
         return id;
     }
@@ -28,6 +38,13 @@ public class Patient {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getEmail(){
         return this.email;
     }
@@ -40,6 +57,27 @@ public class Patient {
     }
     public void setDoctor(Doctor doctor) {
         this.doctor = doctor;
+    }
+
+    public Prescription[] getPrescriptions(){
+        return prescriptions;
+    }
+    public void setPrescriptions(Prescription[] prescriptions){
+        this.prescriptions = prescriptions;
+    }
+
+    public String[] getCondition(){
+        return condition;
+    }
+    public void setCondition(String[] condition){
+        this.condition = condition;
+    }
+
+    public String[] getSymptoms(){
+        return symptoms;
+    }
+    public void setSymptoms(String[] symptoms){
+        this.symptoms = symptoms;
     }
 
 
