57ca8fc5d59bca3106c2856e580ee86f2e7702f9
KianSalehi <kian.k.salehi@gmail.com>
2021-01-29 22:35:24 -0500

    Patient login, Doctor login, Patient Sign up added

---
+++ 711/app/src/main/java/com/example/a711/PatientSignup.java
@@ -0,0 +1,14 @@
+package com.example.a711;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_signup);
+    }
+}
