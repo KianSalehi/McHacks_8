37be313df74d48fd544ba065d8fbf9d7b37ecf8a
Adel-Ab <45152570+Adel-Ab@users.noreply.github.com>
2021-01-30 15:08:36 -0500

    Create Appointment.java

---
+++ 711/app/src/main/java/com/example/a711/Appointment.java
@@ -0,0 +1,56 @@
+package com.example.a711;
+
+public class Appointment {
+
+    // Appointment informations
+    private Patient patient;
+    private Doctor doctor;
+    private String date;
+    private String time;
+    private String notes;
+    // Constructor
+    public Appointment(Patient patient, Doctor doctor, String date, String time, String notes) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+        this.time = time;
+        this.notes = notes;
+    }
+
+    // Getter Setter
+    public Patient getPatient() {
+        return patient;
+    }
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
+
