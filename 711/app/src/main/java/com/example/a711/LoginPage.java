a3abe9242ac20c222cc04ecd70909324ef76604d
KianSalehi <kian.k.salehi@gmail.com>
2021-01-29 23:43:36 -0500

    updated

--- 711/app/src/main/java/com/example/a711/LoginPage.java
+++ 711/app/src/main/java/com/example/a711/LoginPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DoctorLogin extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doctor_login);
+        setContentView(R.layout.activity_login);
     }
 }
