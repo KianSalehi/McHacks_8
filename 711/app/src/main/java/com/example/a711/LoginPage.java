592c4ce80396c14c20c43e79e1de2cdf5266a141
KianSalehi <kian.k.salehi@gmail.com>
2021-01-30 00:17:27 -0500

    updated

--- 711/app/src/main/java/com/example/a711/LoginPage.java
+++ 711/app/src/main/java/com/example/a711/LoginPage.java
@@ -1,14 +1,117 @@
 package com.example.a711;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPage extends AppCompatActivity {
-
+    private static final String TAG = "LoginPage.java :";
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        Button login = (Button) findViewById(R.id.LoginPageLoginButton);
+        final TextView email = (TextView) findViewById(R.id.LoginPageEmail);
+        final TextView pass = (TextView) findViewById(R.id.LoginPagePassword);
+        Button loginDoctor = (Button) findViewById(R.id.LoginPageDoctorLogin);
+        loginDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logInDoctor(email.getText().toString(),pass.getText().toString());
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn(email.getText().toString(), pass.getText().toString());
+            }
+        });
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null){
+            Toast.makeText(this, "You Signed In successfully!", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, PatientHomePage.class));
+        }
+        else{
+            Toast.makeText(this, "Could Not Sign In!", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void updateUIDoctor(FirebaseUser currentUser){
+        if (currentUser != null){
+            Toast.makeText(this, "You Signed In successfully!", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, DoctorHomePage.class));
+        }
+        else{
+            Toast.makeText(this, "Could Not Sign In!", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void logIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginPage.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    public void logInDoctor(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUIDoctor(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginPage.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUIDoctor(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
--- 711/app/src/main/java/com/example/a711/PatientSignup.java
+++ 711/app/src/main/java/com/example/a711/PatientSignup.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class PatientSignup extends AppCompatActivity {
     private static final String TAG = "PatientSignup.java :";
@@ -28,23 +29,27 @@ public class PatientSignup extends AppCompatActivity {
         Button signUp= (Button) findViewById(R.id.PatientSignupButton);
         final TextView email = (TextView) findViewById(R.id.PatientSignupEmail);
         final TextView password = (TextView) findViewById(R.id.PatientSignupPass);
+        final TextView name = (TextView) findViewById(R.id.PatientSignupFirstLastName);
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signUp(email.getText().toString(), password.getText().toString());
+                signUp(email.getText().toString(), password.getText().toString(), name.getText().toString());
             }
         });
     }
-    public void onStart() {
-        super.onStart();
+//    public void onStart() {
+  //      super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
+   //     FirebaseUser currentUser = mAuth.getCurrentUser();
+ //       updateUI(currentUser);
+ //   }
 
-    private void updateUI(FirebaseUser currentUser) {
+    private void updateUI(FirebaseUser currentUser, String name) {
         //Switch to homepage for patients
         if (currentUser!=null){
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(name).build();
+            currentUser.updateProfile(profileUpdates);
             Toast.makeText(this, "You Signed In successfully!", Toast.LENGTH_LONG).show();
             startActivity(new Intent(PatientSignup.this, PatientHomePage.class));
     }
@@ -54,7 +59,7 @@ public class PatientSignup extends AppCompatActivity {
     }
 
     //Sign up function
-    public void signUp(String email, String password){
+    public void signUp(String email, String password, final String name){
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(PatientSignup.this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -63,13 +68,13 @@ public class PatientSignup extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+                            updateUI(user, name);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
                             Toast.makeText(PatientSignup.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+                            updateUI(null, null);
                         }
 
                     }
